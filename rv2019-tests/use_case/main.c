8746193572d468d9dded3367488c9e25892a1e2c
Chun Tian (binghe) <binghe.lisp@gmail.com>
2019-05-17 22:26:33 +0200

    Align the test code (main.c) with paper

--- rv2019-tests/use_case/main.c
+++ rv2019-tests/use_case/main.c
@@ -3,10 +3,13 @@
 
 int main(int argc, char **argv)
 {
-  int location;
+  int monitor_loc, out;
 
-  printf("%d == 0 (unknown)\n", M0(1 /* p */, 1 /* hard */, &location));
-  printf("%d == 1 (true)\n",    M0(2 /* q */, 0 /* none */, &location));
+  out = M0 (0b01 /* p & !q */, 1 /* hard */, &monitor_loc);
+  printf("%d == 0 (unknown)\n", out);
+
+  out = M0 (0b10 /* !p & q */, 0 /* none */, &monitor_loc);
+  printf("%d == 1 (true)\n", out);
 
   return 0;
 }
