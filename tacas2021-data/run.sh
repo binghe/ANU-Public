2466fbc5cadaa9cbf77e2682482a4882cbec0f92
Chun Tian (binghe) <binghe.lisp@gmail.com>
2020-10-16 11:00:29 +0200

    Fixed test scripts for Linux (sh = bash)

--- tacas2021-data/run.sh
+++ tacas2021-data/run.sh
@@ -1,11 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+
+mkdir -p output
 
 for n in {0..54}; do
 
 cat > temp.cmd <<EOF
 go_msat
 read_trace traces/trace-1.xml
-verify_property -x -n ${n} -o output.csv 1.1:20
+show_property -n ${n}
+verify_property -x -n ${n} -o output/output-${n}.csv 1.1:20
 quit
 EOF
 
